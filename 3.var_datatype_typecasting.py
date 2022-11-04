63e5ca3ff0f7e1b229661ab8c71326bbe63207f4
Minhaj28 <jucse28.368@gmail.com>
2022-11-05 01:01:43 +0600

    5-11-22

---
+++ 3.var_datatype_typecasting.py
@@ -0,0 +1,14 @@
+var1 = "hello world"
+print(var1)
+var2=4
+var3=4.8
+print(type(var3))
+
+var5="10000000000000000000000000000000000004"
+var6="6"
+print(int(var5)+int(var6))
+print(10*"minhaj\n")
+
+print("Enter a number: ",end="")
+input=input()
+print("The number is = ",int(input)+7)
