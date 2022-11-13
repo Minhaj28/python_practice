b5dc87e3dd0d5e11d75c8df67f59fddeedf5cfea
Minhaj28 <jucse28.368@gmail.com>
2022-11-14 00:58:45 +0600

    14-11-22

--- 5.list.py
+++ 5.list.py
@@ -1,3 +1,14 @@
-Class =["one","two","three","four",5,6,7]
-print(Class)
-print(Class[6])
+# Class =["one","two","three","four",5,6,7]
+# print(Class)
+# print(Class[6])
+numbers = [2,3,7,9,11]
+# numbers.sort()
+# print(numbers[3:4])
+# print(max(numbers))
+# numbers.append(15)
+# numbers.insert(2,30)
+# tp = (1,2,4,5,6)
+tp = (1,)
+
+
+print(tp)
---
+++ 6.Dictionary.py
@@ -0,0 +1,6 @@
+d1 = {"minhaj":"3","robi":"5","minhaj":"4","sakib":{"cn":"98","hci":"87"}}
+# print(d1["sakib"])
+print(d1["sakib"]["cn"])
+del d1["robi"]
+print(d1)
+ 
