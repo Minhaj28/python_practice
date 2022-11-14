8d32746b6cf637fbb7adaa1a748ba53d6ff45cca
Minhaj28 <jucse28.368@gmail.com>
2022-11-15 00:20:40 +0600

    15-11-22

---
+++ 8.if_elif_else.py
@@ -0,0 +1,21 @@
+# v1 = 6
+# v2 = 50
+
+# v3 = int(input())
+
+# if v3>v2:
+#     print("greter")
+# elif v3==v2:
+#     print("Equal")
+# else:
+#     print("lesser")
+li = [2,3,6]
+
+if 5 in li:
+    print("5 in list")
+else:
+    print("5 not in list")
+
+age = int(input("Enter your age: "))
+print(type(age))
+print(age)
---
+++ 9.function.py
@@ -0,0 +1,10 @@
+# a = 9
+# b = 8
+# c = sum((a,b))
+# print(c)
+
+def func():
+    print("minhaj")
+
+# func()
+print(func())
