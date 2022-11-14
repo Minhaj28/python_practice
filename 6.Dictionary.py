b8f4061904c4ccb58386bc1666aa677181ba64a0
Minhaj28 <jucse28.368@gmail.com>
2022-11-15 00:20:40 +0600

    15-11-22

--- 6.Dictionary.py
+++ 6.Dictionary.py
@@ -1,6 +1,11 @@
 d1 = {"minhaj":"3","robi":"5","minhaj":"4","sakib":{"cn":"98","hci":"87"}}
 # print(d1["sakib"])
-print(d1["sakib"]["cn"])
-del d1["robi"]
-print(d1)
- 
+# print(d1["sakib"]["cn"])
+# del d1["robi"]
+# print(d1)
+
+name = input("Enter the name: ")
+if name in d1:
+    print(d1[name])
+else:
+    print("Not found")
